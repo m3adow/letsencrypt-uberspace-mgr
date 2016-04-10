e7cf105ff7731a67f75ce02ef751893ce0f3f33f
m3adow_uberspace <mail-github.com@till-wiese.de>
2016-04-10 08:27:11 +0200

    First "final" version

--- letsencrypt-mgr.sh
+++ letsencrypt-mgr.sh
@@ -5,20 +5,24 @@ set -o pipefail
 set -x
 
 LE_CONFIG_DIR=${LE_CONFIG_DIR:-${HOME}/.config/letsencrypt/}
-DOMAINS_FILE=${DOMAINS_FILE:-${HOME}/etc/letsencrypt_domains.conf}
-DOMAIN_PLACEHOLDER=${DOMAIN_PLACEHOLDER:-%DOMAINS%}
-
 LE_CONF=${LE_CONF:-"${LE_CONFIG_DIR}/cli.ini"}
-LE_PATH=${LE_PATH:-"/usr/local/bin/letsencrypt"}
+LE_ARGS=${LE_ARGS:-"--agree-tos --non-interactive"}
+
+DOMAINS_FILE=${DOMAINS_FILE:-${HOME}/etc/letsencrypt_domains.conf}
+
+# Include Path to Uberspace scripts
+PATH="${PATH}:/usr/local/bin/"
 
 while read -u 42 DOMAIN_LINE
 do
 	# skip comments
 	[[ "${DOMAIN_LINE:0:1}" == '#' ]] && continue
 
+	# Save original cli.ini
 	sed -i".sic" "/^domains =/ c\
 		domains = ${DOMAIN_LINE}" "${LE_CONF}"
-	${LE_PATH} certonly
+	letsencrypt certonly
+	# Restore original cli.ini
 	mv "${LE_CONF}.sic" "${LE_CONF}"
 	LIVE_DIR="${LE_CONFIG_DIR}/live/${DOMAIN_LINE%%,*}/"
 	uberspace-add-certificate -k "${LIVE_DIR}/privkey.pem" -c "${LIVE_DIR}/cert.pem"
